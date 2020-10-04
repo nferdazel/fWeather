360aca5c4b03a3595d0b4fc5c3765f0be2d911dd
atoleco <fredianto@tutanota.com>
2020-10-04 19:07:28 +0700

    changed api key

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,30 +1,26 @@
 import 'package:flutter/material.dart';
-
-import '../services/location.dart';
+import 'package:fWeather/services/location.dart';
 import 'package:fWeather/services/network.dart';
-import 'package:fWeather/utilities/constants.dart';
+import 'package:fWeather/config.dart';
 import 'package:weather_icons/weather_icons.dart';
 
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-const openWeatherURL='https://api.openweathermap.org/data/2.5/weather';
+const openWeatherURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
-
-  Future<dynamic> cityWeather(String cityName)async{
-    var URL='$openWeatherURL?q=$cityName&appid=$kAPI&units=metric';
-    NetworkHelper networkHelper=NetworkHelper(URL);
-    var weatherData=await networkHelper.getData();
+  Future<dynamic> cityWeather(String cityName) async {
+    var apiUrl = '$openWeatherURL?q=$cityName&appid=$apiKey&units=metric';
+    NetworkHelper networkHelper = NetworkHelper(apiUrl);
+    var weatherData = await networkHelper.getData();
     return weatherData;
   }
 
-  Future<dynamic> locationWeather()async{
+  Future<dynamic> locationWeather() async {
     Location location = new Location();
     await location.getCurrentLocation();
+    NetworkHelper networkhelper = NetworkHelper(
+        '$openWeatherURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+    var weatherData = await networkhelper.getData();
 
-    NetworkHelper networkhelper =NetworkHelper('$openWeatherURL?lat=${location.latitude}&lon=${location.longitude}&appid=$kAPI&units=metric');
-
-    var weatherData=await networkhelper.getData();
     return weatherData;
   }
 
@@ -39,49 +35,63 @@ class WeatherModel {
       return 'Bring a 🧥 & ☂ (just in case)';
     }
   }
- Icon getWeatherIcon(int condition){
+
+  Icon getWeatherIcon(int condition) {
     if (condition < 300) {
-      return Icon(WeatherIcons.thunderstorm,color:Colors.red,);
+      return Icon(
+        WeatherIcons.thunderstorm,
+        color: Colors.blue,
+      );
     } else if (condition < 400) {
-      return Icon(WeatherIcons.rain_mix,color:Colors.red,);
+      return Icon(
+        WeatherIcons.rain_mix,
+        color: Colors.blue,
+      );
     } else if (condition < 600) {
-      return Icon(WeatherIcons.rain,color:Colors.red,);
+      return Icon(
+        WeatherIcons.rain,
+        color: Colors.blue,
+      );
     } else if (condition < 700) {
-      return Icon(WeatherIcons.snowflake_cold,color:Colors.red,);
+      return Icon(
+        WeatherIcons.snowflake_cold,
+        color: Colors.blue,
+      );
     } else if (condition < 800) {
-
-      return Icon(WeatherIcons.day_windy,color:Colors.red,);
+      return Icon(
+        WeatherIcons.day_windy,
+        color: Colors.blue,
+      );
     } else if (condition == 800) {
-      return Icon(WeatherIcons.day_sunny,color:Colors.red,);
+      return Icon(
+        WeatherIcons.day_sunny,
+        color: Colors.blue,
+      );
     } else if (condition <= 804) {
-      return Icon(WeatherIcons.cloud,color:Colors.red,);
-    } else
-    return null;
-  }
-  String subtext(int condition){
-    if (condition < 300) {
-      return "Thunderstorm";
-
-    } else if (condition < 400) {
-      return "Drizzle";
-
-    } else if (condition < 600) {
-      return "Hmm its Raining";
-
-    } else if (condition < 700) {
-      return "Snow";
-
-    } else if (condition < 800) {
-      return "Wind's Howling";
-
-    } else if (condition == 800) {
-      return "Sunny";
-
-    } else if (condition <= 804) {
-      return "Cloudy";
-
+      return Icon(
+        WeatherIcons.cloud,
+        color: Colors.blue,
+      );
     } else
       return null;
+  }
 
+  String subText(int condition) {
+    if (condition < 300) {
+      return "Thunderstorm";
+    } else if (condition < 400) {
+      return "Drizzle";
+    } else if (condition < 600) {
+      return "Hmm its Raining";
+    } else if (condition < 700) {
+      return "Snow";
+    } else if (condition < 800) {
+      return "Wind's Howling";
+    } else if (condition == 800) {
+      return "Sunny";
+    } else if (condition <= 804) {
+      return "Cloudy";
+    } else
+      return null;
   }
 }
